c6bcd5c3824950ad4d4cb26bf139d66ac6be3b70
Administrator <mehrdadbahrainy@gmail.com>
2023-12-11 03:04:36 +0330

    create database tables

---
+++ OnlineStore.Database/dbo/Tables/Category.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Category]
+(
+	[Id]		        INT				NOT NULL    IDENTITY(1000,1), 
+    [Name]		        NVARCHAR(50)	NOT NULL,
+    [UserId]            INT             NOT NULL, 
+    [EntryDate]         DATETIME2       NOT NULL    CONSTRAINT  [DF_Category_EntryDate]	DEFAULT	(SYSUTCDATETIME()),  
+    [IsActive]          BIT             NOT NULL    CONSTRAINT  [DF_Category_IsActive] DEFAULT ((0)), 
+    [IsDeleted]         BIT             NOT NULL    CONSTRAINT  [DF_Category_IsDeleted] DEFAULT ((0)), 
+
+    CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Category_User] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User]([Id]),
+)
---
+++ OnlineStore.Database/dbo/Tables/User.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id]                INT                 NOT NULL    IDENTITY(1000,1), 
+    [Username]          NVARCHAR(50)        NOT NULL, 
+    [Password]          NVARCHAR(50)        NOT NULL, 
+    [PasswordSalt]      NVARCHAR(50)        NOT NULL, 
+    [FirstName]         NVARCHAR(30)        NOT NULL, 
+    [LastName]          NVARCHAR(30)        NULL, 
+    [Email]             NVARCHAR(50)        NOT NULL, 
+    [BirthDate]         DATETIME2           NULL, 
+    [IsActive]          BIT                 NOT NULL    CONSTRAINT  [DF_User_IsActive] DEFAULT ((0)), 
+    [EntryDate]         DATETIME2           NOT NULL    CONSTRAINT  [DF_User_EntryDate]	DEFAULT	(SYSUTCDATETIME()), 
+    [IsDeleted]         BIT                 NOT NULL    CONSTRAINT  [DF_User_IsDeleted] DEFAULT ((0)), 
+
+    CONSTRAINT  [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC),    
+)
